484bafe06d4cdb36da84f1cc9a57faeeab761b7f
Rajan Khullar <rkhullar03@gmail.com>
2018-02-04 17:43:01 -0500

    implemented test runner

--- TestRunner.java
+++ TestRunner.java
@@ -1,7 +1,37 @@
+import org.junit.runner.JUnitCore;
+        import org.junit.runner.Result;
+        import org.junit.runner.notification.Failure;
+
+        import java.util.HashMap;
+
 class TestRunner
 {
+
+    private static final int max = 10, min = 2;
+    private static final HashMap<String, Integer> penalties = new HashMap<>();
+
+    static
+    {
+        penalties.put("test_hello_1", 5);
+        penalties.put("test_hello_2", 5);
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("running tests");
+        Result result = JUnitCore.runClasses(HelloTest.class);
+
+        int grade = max;
+
+        for(Failure failure: result.getFailures())
+        {
+            String name = failure.getTestHeader().split("[(]")[0];
+            System.err.printf("failed test: %s\n", name);
+            grade -= penalties.get(name);
+        }
+
+        if(grade < min)
+            grade = min;
+
+        System.out.println(grade);
     }
 }
